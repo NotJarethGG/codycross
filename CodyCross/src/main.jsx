4ec3394b91d79a594a2be26842d3f88643cd4799
Jareth Moraga <111523122+NotJarethGG@users.noreply.github.com>
2026-03-15 17:07:27 -0600

    Update main.jsx

--- CodyCross/src/main.jsx
+++ CodyCross/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cody from './Cody.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Cody />  
-     {/* <App /> */}
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"    element={<Cody />} />
+        <Route path="/404" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
